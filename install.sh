85d02939ad0ed35d175516a66008c577db231910
Julian Bilcke <julian.bilcke@iscpif.fr>
2011-06-09 14:25:43 +0200

    updated processing

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 cd misc
 mvn install:install-file -DgroupId=netscape -DartifactId=javascript -Dversion=1.6.0_22 -Dpackaging=jar -Dfile=plugin.jar -DgeneratePom=true
 mvn install:install-file -DgroupId=traer -DartifactId=physics -Dversion=3.0 -Dpackaging=jar -Dfile=physics.jar -DgeneratePom=true
-mvn install:install-file -DgroupId=org.processing -DartifactId=itext -Dversion=1.0 -Dpackaging=jar -Dfile=itext.jar -DgeneratePom=true
-mvn install:install-file -DgroupId=org.processing -DartifactId=pdf -Dversion=1.0 -Dpackaging=jar -Dfile=pdf.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.processing -DartifactId=itext -Dversion=1.5.1 -Dpackaging=jar -Dfile=itext.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.processing -DartifactId=pdf -Dversion=1.5.1 -Dpackaging=jar -Dfile=pdf.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.processing -DartifactId=opengl -Dversion=1.5.1 -Dpackaging=jar -Dfile=opengl.jar -DgeneratePom=true
+mvn install:install-file -DgroupId=org.processing -DartifactId=core -Dversion=1.5.1 -Dpackaging=jar -Dfile=core.jar -DgeneratePom=true
 cd ..
+
