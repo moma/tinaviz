f0b75c8d0a67c841bb9f2671946180340ef3e94c
Julian bilcke <julian.bilcke@gmail.com>
2011-08-22 15:29:00 +0100

    added misc/test files

---
+++ misc/test2.sh
@@ -0,0 +1,1 @@
+mvn deploy:deploy-file -Durl=http://maven.iscpif.fr/gephi-third-party -DrepositoryId=gephi-thirdparty -Dfile=core.jar -DgroupId=org.processing -DartifactId=core -Dversion=1.5.1 -Dpackaging=jar -DgeneratePom=true -e -X
