3ce531f5d11da35ce1079095a3f957134817d024
Julian Bilcke <julian.bilcke@daizoru.com>
2012-02-14 19:07:39 +0100

    Update src/main/scala/eu/tinasoft/tinaviz/io/Webpage.scala

--- src/main/scala/eu/tinasoft/tinaviz/io/Webpage.scala
+++ src/main/scala/eu/tinasoft/tinaviz/io/Webpage.scala
@@ -59,7 +59,8 @@ class Webpage(val session: Session) extends Actor {
 
         case ('download, str: String) =>
           println("sending file to the browser..")
-          val base64ified = "data:text/xml;subtype=\"gexf/1.2\";base64," + Base64.encode(str)
+          val base64ified = "data:application/octet-stream;base64," + Base64.encode(str)
+          //val base64ified = "data:text/xml;subtype=\"gexf/1.2\";base64," + Base64.encode(str)
           window.call("open", Array[Object](base64ified, "_newtab", new java.lang.Integer(0)))
 
         case (cb:Int, data:Map[String,Any]) =>
